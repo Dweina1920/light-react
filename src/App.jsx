839d9666c6d13111f098249da9c9ec560a01a4e8
Dina <dina.yamanid@gmail.com>
2023-05-25 14:22:18 +0200

    light done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import './App.css'
-import LightOnImg from './assets/lighton.png'
-import LightOffImg from './assets/lightoff.png'
+import "./App.css";
+import LightOnImg from "./assets/lighton.png";
+import LightOffImg from "./assets/lightoff.png";
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
       <Light />
     </>
-  )
+  );
 }
 
 function Light() {
+  const [lightOn, setLightOn] = useState(false);
 
-  return <div>
-    <h1>Click the buttons to turn on or off the light</h1>
-    <img src={LightOnImg} />
-    <div><button>Turn On</button><button>Turn Off</button></div>
-  </div>
+  function toggleButton() {
+    setLightOn(!lightOn);
+  }
+
+
+  return (
+    <div>
+      <h1>Click the buttons to turn on or off the light</h1>
+      <img src={lightOn ? LightOnImg : LightOffImg} />
+      <div>
+        <button
+          
+          onClick={toggleButton}
+        >
+         Turn { lightOn? "Off" : "On"} 
+        </button>
+    
+        
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
